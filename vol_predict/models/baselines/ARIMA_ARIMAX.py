08dfdf1db6afcb1d516ca4039021d935d844ba5b
m-imris <marcus.imris@icloud.com>
2025-04-03 17:39:31 +0200

    Tidied up the code

--- vol_predict/models/baselines/ARIMA_ARIMAX.py
+++ vol_predict/models/baselines/ARIMA_ARIMAX.py
@@ -7,27 +7,15 @@ import statsmodels.api as sm
 import pandas as pd
 import numpy as np
 
-#Loading test dataset
-from pmdarima.datasets.stocks import load_msft
-
-data_set = load_msft()
-
-df_open = data_set["Open"]
-log_open = np.log(df_open)
-split_index = int(len(df_open) * 0.7)
-train = log_open[:split_index]
-test = log_open[split_index:]
-
-
+#Plots acf and pacf side by side
 def plot_acf_pacf(df):
     fig, ax = plt.subplots(1,2)
     sm.graphics.tsa.plot_acf(df, ax=ax[0])
     sm.graphics.tsa.plot_pacf(df, ax=ax[1])
     plt.show()
 
-#plot_acf_pacf(log_open)
 
-#Determines the d using the Augmented Dickey–Fuller test 
+#Determines the optimal d using the Augmented Dickey–Fuller test 
 def determine_d(df, alpha, max_d):
     d = 0
     current_df = df.copy()
@@ -39,9 +27,6 @@ def determine_d(df, alpha, max_d):
         d += 1
     return d
 
-d = determine_d(train, 0.01, 5)
-
-#plot_acf_pacf(log_open.diff().dropna())
 
 #Determines the p and q that minimizes the AIC
 def find_p_q(ts, d, max_p, max_q, exog, exog_df):
@@ -72,9 +57,7 @@ def find_fit(ts, alpha, max_d, max_p, max_q, exog, exog_df = None):
         best_model = ARIMA(train, order=(best_p, d, best_q))
     return best_model.fit(method_kwargs={'maxiter':1000})
 
-
-best_fit = find_fit(train, 0.01, 5, 5, 5, False)
-
+#Plots the residuals including their density
 def plot_residuals(model):
     resids = model.resid[1:]
     fig, ax = plt.subplots(1,2)
@@ -83,14 +66,20 @@ def plot_residuals(model):
     plt.show()
 
 
+'''
+#Test data set I used when implementing the model
 
-def plot_resids_acf_pacf(model):
-    resids = model.resid[1:]
-    fig, ax = plt.subplots(1,2)
-    sm.graphics.tsa.plot_acf(resids, ax=ax[0])
-    sm.graphics.tsa.plot_pacf(resids, ax=ax[1])
-    plt.show()
+from pmdarima.datasets.stocks import load_msft
 
+data_set = load_msft()
+
+df_open = data_set["Open"]
+log_open = np.log(df_open)
+split_index = int(len(df_open) * 0.7)
+train = log_open[:split_index]
+test = log_open[split_index:]
+
+best_fit = find_fit(train, 0.01, 5, 5, 5, False)
 
 train_pred = best_fit.predict(start = 1, end = len(train) - 1)
 forecast_returns = best_fit.forecast(steps = len(test))
@@ -102,3 +91,4 @@ plt.plot(forecast_returns.index, forecast_returns, label="Out-of-Sample Forecast
 plt.title("ARIMA Model: True vs. Predicted Values (Log Returns)")
 plt.legend()
 plt.show()
+'''
